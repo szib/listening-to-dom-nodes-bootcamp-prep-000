40e2a8a25de421f9215a789bdf08e4bb7e768e1b
Ivan Szebenszki <szib77@gmail.com>
2019-02-28 23:39:58 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,8 +6,12 @@ main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
 
-const input = document.querySelector('input');
+const input = document.querySelector('input')
  
 input.addEventListener('keydown', function(e) {
-  console.log(e.key);
+  if (e.key === "g") {
+    return e.preventDefault()
+  } else {
+    console.log(e.key)
+  }
 });
